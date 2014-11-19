8824999f3b3490a0691119d4b12c9e281afbd64a
aqingsao <vinci.zhang@gmail.com>
2014-11-19 15:44:43 +0800

    add sample data for categories

--- angular/categories/categories.js
+++ angular/categories/categories.js
@@ -5,7 +5,11 @@
   .controller('CategoriesCtrl', CategoriesCtrl)
 
   /* @ngInject */
-  function CategoriesCtrl(){
+  function CategoriesCtrl($scope){
+    $scope.categories = [
+      new Category(1, '新鲜水果', '苹果/柠檬/橙子/石榴/梨', 'http://www.tongshijia.com/categories/tag/shuiguoganguo.html', 'http://www.tongshijia.com/img/class/class3.jpg'),
+      new Category(2, '枣、干果', '枸杞/核桃/葡萄干/无花果/枣', 'http://www.tongshijia.com/categories/tag/xiaochilingshi.html', 'http://www.tongshijia.com/img/class/class5.jpg')
+    ]
   }
 
 })(window, window.angular);
--- angular/models.js
+++ angular/models.js
@@ -5,3 +5,19 @@ var Sharing = function(id, product, name, link, img){
   this.link = link;
   this.img = img;
 };
+
+var Trying = function(id, title, summary, link, imgSrc){
+  this.id = id;
+  this.title = title;
+  this.summary = summary;
+  this.link = link;
+  this.imgSrc = imgSrc;
+};
+
+var Category = function(id, title, explain, link, imgSrc){
+  this.id = id;
+  this.title = title;
+  this.explain = explain;
+  this.link = link;
+  this.imgSrc = imgSrc;
+};
