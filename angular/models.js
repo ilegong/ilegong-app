58a7ed2a549b7e94afbb9d7abc2d3f6fe256302e
aqingsao <vinci.zhang@gmail.com>
2014-11-19 13:10:17 +0800

    add sample data for sharings

---
+++ angular/models.js
@@ -0,0 +1,7 @@
+var Sharing = function(id, product, name, link, img){
+  this.id = id;
+  this.product = product;
+  this.name = name;
+  this.link = link;
+  this.img = img;
+};
--- angular/sharings/sharings.js
+++ angular/sharings/sharings.js
@@ -5,7 +5,11 @@
   .controller('SharingsCtrl', SharingsCtrl)
 
   /* @ngInject */
-  function SharingsCtrl(){
+  function SharingsCtrl($scope){
+    $scope.sharings = [
+      new Sharing(1, '阿里巴巴', '李瑞', 'http://www.tongshijia.com/brands/20141117/qu_na_er_li_rui.html', 'http://51daifan-images.stor.sinaapp.com/files/201411/thumb_m/858bbafb0fb_1117.jpg'), 
+      new Sharing(2, '山西核桃', '刘富明', 'http://www.tongshijia.com/brands/20141117/shan_xi_he_tao_liu_fu_ming.html', 'http://51daifan-images.stor.sinaapp.com/files/201411/thumb_m/ee4ff13b4d8_1117.jpg')
+    ]
   }
 
 })(window, window.angular);
