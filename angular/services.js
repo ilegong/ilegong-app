3dac5c650840da8f6474c09d82464e88c6da73a1
aqingsao <vinci.zhang@gmail.com>
2014-12-05 15:01:27 +0800

    add data.js for sample data

--- angular/services.js
+++ angular/services.js
@@ -6,6 +6,7 @@
   .value('software', {app: {id: '201411290001', name: 'ailegong', version: ''}, server: {address: 'http://www.tongshijia.com', port: 80}})
   .service('Base', Base)
   .service('Products', Products)
+  .service('Categories', Categories)
 
   /* @ngInject */
   function Base($http, $q, software){
@@ -16,7 +17,11 @@
     }
 
     function get(url){
-      return $http.get(software.server.address + url);
+      // return $http.get(software.server.address + url);
+      console.log("get data for " + url);
+      var defer = $q.defer();
+      defer.resolve(data[url]);
+      return defer.promise;
     }
 
     function deferred(data){
@@ -35,75 +40,18 @@
     }
 
     function list(){
-      // return Base.get('/categories/mobileHome.json');
-      var sample = {  
-        "brands":{  
-          "8":{  
-             "Brand":{  
-                "id":"8",
-                "name":"\u6700\u65b0\u63a8\u8350",
-                "created":"2014-01-27 09:52:28",
-                "slug":"",
-                "coverimg":"http:\/\/51daifan-images.stor.sinaapp.com\/files\/201401\/thumb_m\/2eb8846d77e_0127.jpg"
-             }
-          }
-        },
-        "tagsWithProducts":[  
-          {  
-            "ProductTag":{  
-              "id":"3",
-              "name":"\u65b0\u9c9c\u6c34\u679c",
-              "slug":"shuiguoganguo",
-              "priority":"900",
-              "show_in_home":"1",
-              "published":true,
-              "created":null,
-              "size_in_home":"8",
-              "coverimg":"\/img\/class\/classn3.png"
-            },
-           "Products":[  
-              {  
-                "id":"302",
-                "name":"\u5e73\u548c\u742f\u6eaa\u7ea2\u5fc3\u871c\u67da",
-                "created":"2014-11-26 12:31:00",
-                "brand_id":"98",
-                "coverimg":"http:\/\/51daifan-images.stor.sinaapp.com\/files\/201411\/thumb_m\/a293b157a39_1126.jpg",
-                "promote_name":"\u4ec5\u9650\u5317\u4eac\u5929\u6d25\u6cb3\u5317\u5730\u533a\uff0c\u665a\u4e94\u70b9\u524d\u4e0b\u5355\u7b2c\u4e8c\u5929\u5230\u8d27\u3002\u516c\u53f8\u5730\u5740\u4e70\u5bb6\u6211\u4eec\u5c06\u5728\u5de5\u4f5c\u65e5\u914d\u9001\uff0c\u907f\u514d\u653e\u7f6e\u65f6\u95f4\u8fc7\u957f\u3002",
-                "comment_nums":"2",
-                "price":"35",
-                "original_price":null,
-                "slug":"hong_xin_you_zi"
-              },
-              {  
-                "id":"201",
-                "name":"\u6cb3\u5357\u6cb3\u9634\u8f6f\u7c7d\u77f3\u69b45\u65a4",
-                "created":"2014-10-13 17:54:00",
-                "brand_id":"57",
-                "coverimg":"http:\/\/51daifan-images.stor.sinaapp.com\/files\/201411\/thumb_s\/03ca7900316_1104.jpg",
-                "promote_name":"",
-                "comment_nums":"34",
-                "price":"55",
-                "original_price":"105",
-                "slug":"he_nan_xing_yang_he_yin_ruan_zi_shi_liu_5jin_6_7liang"
-              },
-              {  
-                "id":"318",
-                "name":"\u6500\u679d\u82b1\u7ea2\u683c\u7ebd\u8377\u5c14\u8110\u6a59",
-                "created":"2014-12-02 16:22:00",
-                "brand_id":"107",
-                "coverimg":"http:\/\/51daifan-images.stor.sinaapp.com\/files\/201412\/thumb_m\/8124f7fb16a_1202.jpg",
-                "promote_name":"\u9664\u65b0\u7586\u3001\u897f\u85cf\u3001\u5185\u8499\u53e4\u3001\u4e1c\u4e09\u7701\u4e0d\u5305\u90ae\uff0c\u5168\u56fd\u5176\u4ed6\u5730\u65b9\u5747\u5305\u90ae~",
-                "comment_nums":"1",
-                "price":"66",
-                "original_price":null,
-                "slug":"pan_zhi_hua_hong_ge_niu_he_er_qi_cheng"
-              }
-            ],
-          }
-        ], 
-        "sub_title":null
-      };
-      return Base.deferred(sample);
+      return Base.get('/categories/mobileHome.json');
+    }
+  }
+
+  function Categories(Base){
+    var self = this;
+    return {
+      get: get
+    }
+
+    function get(slug){
+      return Base.get("/categories/tag/" + slug + ".json");
     }
   }
 
