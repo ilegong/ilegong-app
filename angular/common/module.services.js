fbc285185f80a8bb80ca94918db9b0efc6a424ab
aqingsao <vinci.zhang@gmail.com>
2015-01-24 16:39:12 +0800

    update homeJson in data.js

--- angular/common/module.services.js
+++ angular/common/module.services.js
@@ -4,7 +4,7 @@
   angular
   .module('module.services', ['LocalForageModule'])
 
-  .value('software', {fakeData: false , timeout: 2500, app: {client_id: 'NTQ5NTE5MGViMTgzMDUw', name: 'ailegong', version: ''}, server: {address: 'http://www.tongshijia.com'}})
+  .value('software', {fakeData: true , timeout: 2500, app: {client_id: 'NTQ5NTE5MGViMTgzMDUw', name: 'ailegong', version: ''}, server: {address: 'http://www.tongshijia.com'}})
 
   .service('Base', Base)
   /* @ngInject */
