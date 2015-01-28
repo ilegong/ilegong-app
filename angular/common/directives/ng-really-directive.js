c47654a119376f982722d7a310cca6debee63733
aqingsao <vinci.zhang@gmail.com>
2015-01-28 11:20:55 +0800

    improve ui for popup confirmation dialog

--- angular/common/directives/ng-really-directive.js
+++ angular/common/directives/ng-really-directive.js
@@ -5,20 +5,44 @@
   .module('module.directives')
   .directive('ngReallyClick', ngReallyClick);
 
-  function ngReallyClick($rootScope, $log) {
+  function ngReallyClick($rootScope, $log, $ionicPopup, $timeout) {
+    var confirm = confirm;
     return {
       restrict: 'A',
       link: function($scope, element, attrs) {
         element.bind('click', function() {
           var message = attrs.ngReallyMessage;
-          if (message && confirm(message)) {
-            $scope.$apply(attrs.ngReallyClick);
-          }
+          confirm(message).then(function(result) {
+            if(result){
+              $scope.$applyAsync(attrs.ngReallyClick);
+            }
+          }, function(e){})
         });
         $scope.$on('$destroy', function() {
           element.off('click');
         });
       }
+    }
+
+    function confirm(message){
+      return $ionicPopup.show({
+        title: message,
+        subTitle: '',
+        buttons:[
+          {
+            text:'取消',
+            onTap:function(e){
+              return false;
+            }
+          },
+          {
+            text:'确定',
+            onTap:function(e){
+              return true;
+            }
+          }
+        ]
+      });
     }
   }
 })(window, window.angular);
