2a5147909e3674a25f00a5a929c04b9b845d0280
aqingsao <vinci.zhang@gmail.com>
2015-02-04 15:56:54 +0800

    improve ui performance by removing dom nodes from category detail page

--- angular/categories/controllers/category-detail-controller.js
+++ angular/categories/controllers/category-detail-controller.js
@@ -4,29 +4,31 @@
   angular.module('ilegong.categories')
   .controller('CategoryDetailCtrl', CategoryDetailCtrl)
 
-  function CategoryDetailCtrl($rootScope, $scope, $stateParams, $log, Categories){
+  function CategoryDetailCtrl($rootScope, $scope, $stateParams, $state, $log, Categories){
     var vm = this;
     vm.doRefresh = doRefresh;
+    vm.getBrandById = getBrandById;
+    vm.toProductDetailPage = function(product){$state.go("app.product-detail.intro", {id: product.id,from:-1})};
     active();
 
     function active(){
       vm.slug = $stateParams.slug;
+      vm.brands = $rootScope.brands;
       // ionic bug: http://stackoverflow.com/questions/21257786/angularjs-using-ionic-framework-data-binding-on-header-title-not-working
       vm.name = $stateParams.name;
       Categories.get(vm.slug).then(function(data){
-        vm.brands = data.brands;
-        vm.products = data.data_list;
-        vm.name = data.sub_title;
+        vm.products = _.map(data.data_list, function(product){product.brand = vm.getBrandById(product.brand_id); return product});
       });
     }
     function doRefresh(){
       Categories.get(vm.slug).then(function(data){
-        vm.brands = data.brands;
         vm.products = data.data_list;
-        vm.name = data.sub_title;
       });
       $scope.$broadcast('scroll.refreshComplete');
       $scope.$apply();
+    }
+    function getBrandById(id){
+      return _.find(vm.brands, function(brand){return brand.Brand.id == id});
     }
   }
 })(window, window.angular);
