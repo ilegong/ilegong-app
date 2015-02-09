40cdc773cf8d89d35e3ab60e0acf8f3cccfc256b
aqingsao <vinci.zhang@gmail.com>
2015-02-09 17:08:18 +0800

    fix bug: brand name and image is not shown on orders page

--- angular/my/controllers/my-orders-controller.js
+++ angular/my/controllers/my-orders-controller.js
@@ -6,7 +6,6 @@
 
   function MyOrdersCtrl($log, $scope, $rootScope, $http, $stateParams, $timeout, Orders){
     var vm = this;
-    vm.getBrand = function(brandId){return _.find(vm.brands, function(brand){return brand.Brand.id == brandId})};
     vm.confirmReceivingGoods = confirmReceivingGoods;
     vm.viewLogistics = viewLogistics;
     vm.remindSendingGoods = remindSendingGoods;
@@ -19,11 +18,10 @@
     function activate() {
       vm.state =$stateParams.state;
       vm.orderStateName = Orders.getOrderState(vm.state).name;
-      vm.orders = vm.filterOrders($rootScope.user.orders, vm.state);
-      vm.brands = $rootScope.brands;
+      vm.orders = vm.filterOrders($rootScope.user.orders, $rootScope.brands, vm.state);
       vm.ship_type = $rootScope.user.ship_type;
       $rootScope.$on("orderStateChanged", function(event, order){
-        vm.orders = vm.filterOrders($rootScope.user.orders, vm.state);
+        vm.orders = vm.filterOrders($rootScope.user.orders, $rootScope.brands, vm.state);
       });
     }
     function doRefresh(){
@@ -31,10 +29,10 @@
       $scope.$broadcast('scroll.refreshComplete');
       $scope.$apply();
     }
-    function filterOrders(orders, state){
+    function filterOrders(orders, brands, state){
       var orderOfStates = _.filter($rootScope.user.orders, function(order){return Orders.isOfStates(order, state)});
       return _.map(orderOfStates, function(order){
-        order.Order.brand = vm.getBrand(order.Order.brand_id);
+        order.Order.brand = _.find(brands, function(brand){return brand.Brand.id == order.Order.brand_id});
         order.Order.products =$rootScope.user.order_carts[order.Order.id];
         order.Order.orderState = Orders.getOrderState(order.Order.status);
         return order;
